32eb5d6e098dc1285703b6f62c9857d3ff2a08bc
mhalneami <maan.neami@gmail.com>
2021-12-24 17:52:42 -0500

    add: react-101 project v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      task:"",
+      list:[]
+
+    }
+  }
+  updateTask=(key, value)=>{
+    this.setState({
+      [key]: value
+    })
+  }
+  deleteTasks=()=> {
+    const deletedList = []
+    this.setState({list: deletedList})
+  }
+  addTask=()=> {
+    const newTask={
+      id: 1 + Math.random(),
+      value: this.state.task.slice()
+    };
+    // current list of items
+    const list = [...this.state.list];
+
+    //add new item
+    list.push(newTask);
+
+    //update state
+    this.setState({
+      list,
+      task:""
+    });
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>My list</h1>
+          <button onClick={this.deleteTasks}>Clear list</button>
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                </li>
+              )
+            })}
+          </ul>
+          <input type="text" placeholder="type your text here" onChange={e => this.updateTask("task", e.target.value)}></input>
+          <br/>
+          <button onClick={this.addTask}>Add item</button>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
